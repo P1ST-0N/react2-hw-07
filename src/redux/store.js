7ed08e78fc75b602f529e66306f684c312ac66e5
P1ST-0N <luckylionia@gmail.com>
2024-03-27 23:41:27 +0200

    added filtersSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { filterReducer } from "./filtersSlice";
+import { contactsReducer } from "./contactsSlice";
 
 export const store = configureStore({
   reducer: {
