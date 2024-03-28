53515328ac8e8e660902012dc2eb620eebfee77c
P1ST-0N <luckylionia@gmail.com>
2024-03-28 13:17:13 +0200

    added App structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.module.css";
+import "../../../node_modules/modern-normalize/modern-normalize.css";
+import css from "./App.module.css";
+import { ThreeDots } from "react-loader-spinner";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { selectError, selectLoading } from "../../redux/selectors";
+import { fetchContacts } from "../../redux/contactsOps";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={css.container}>
+      <h1 className={css.mainTitle}>Phonebook</h1>
+      {/* <ContactForm />
+      <SearchBox /> */}
+      <div className={css.containerLoader}>
+        {isLoading && !error && (
+          <ThreeDots
+            visible={true}
+            height="80"
+            width="80"
+            color="#6f6e6e"
+            radius="9"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {/* <ContactList /> */}
+    </div>
   );
 }
 
